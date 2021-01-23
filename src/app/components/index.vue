f352d4dc43b6c43a7324f14cff3211accf8719d2
rockts <rockts@sina.com>
2021-01-24 05:01:41 +0800

    用代码切换路由地址（$router.push 与 $router.replace）

--- src/app/components/index.vue
+++ src/app/components/index.vue
@@ -1,3 +1,14 @@
 <template>
-  <div>你好，欢迎访问宁浩网</div>
+  <div @click="onClickText">你好，欢迎访问宁浩网</div>
 </template>
+
+<script>
+export default {
+  methods: {
+    onClickText() {
+      //   this.$$route.push('/about');
+      this.$route.replace('/about');
+    },
+  },
+};
+</script>
