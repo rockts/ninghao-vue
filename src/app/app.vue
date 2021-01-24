ab1dc841367e2c5de68e2d6ea53f625d807884c3
rockts <rockts@sina.com>
2021-01-25 02:21:20 +0800

    使用 FileReader 生成预览图像

--- src/app/app.vue
+++ src/app/app.vue
@@ -26,6 +26,10 @@
     accept="image/png, image/jpeg, image/jpg"
   />
 
+  <div v-if="imagePreviewUrl">
+    <img class="image-preview" :src="imagePreviewUrl" />
+  </div>
+
   <div>{{ errorMessage }}</div>
   <div v-for="post in posts" :key="post.id">
     <input
@@ -52,6 +56,7 @@ export default {
       title: '',
       currentUser: null,
       file: null,
+      imagePreviewUrl: null,
     };
   },
 
@@ -77,6 +82,16 @@ export default {
   },
 
   methods: {
+    createImagePreview(file) {
+      const reader = new FileReader();
+
+      reader.readAsDataURL(file);
+
+      reader.onload = event => {
+        this.imagePreviewUrl = event.target.result;
+      };
+    },
+
     onChangeFile(event) {
       console.log(event.target.files);
       const file = event.target.files[0];
@@ -85,6 +100,9 @@ export default {
         this.file = file;
 
         this.title = file.name.split('.')[0];
+
+        // 生成预览图像
+        this.createImagePreview(file);
       }
     },
 
