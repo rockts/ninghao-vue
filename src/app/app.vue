8be8eebd8a198b921fc770334ddf7961ee7b3a9e
rockts <rockts@sina.com>
2021-01-24 09:06:42 +0800

    基于 Class 的过渡

--- src/app/app.vue
+++ src/app/app.vue
@@ -6,10 +6,10 @@
         <div class="card-subtitle">Transition & Animation</div>
       </div>
       <div class="card_content">
-        <div class="emoji">🌲</div>
+        <div :class="['emoji', { hidden: !isActive }]">🌲</div>
       </div>
       <div class="card_action">
-        <button>请按这里</button>
+        <button @click="isActive = !isActive">请按这里</button>
       </div>
     </div>
   </div>
@@ -20,6 +20,7 @@ export default {
   data() {
     return {
       name: '宁浩网',
+      isActive: true,
     };
   },
 };
