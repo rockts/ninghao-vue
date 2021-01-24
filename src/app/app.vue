08877d9d10fc0d45365099792ea5c634f5c13ad2
rockts <rockts@sina.com>
2021-01-24 22:59:28 +0800

    发送 POST 类型的 HTTP 请求

--- src/app/app.vue
+++ src/app/app.vue
@@ -15,6 +15,11 @@ export default {
       name: '宁浩网',
       posts: [],
       errorMessage: '',
+      user: {
+        name: '高鹏',
+        password: '123123',
+      },
+      token: '',
     };
   },
 
@@ -25,6 +30,16 @@ export default {
       // console.log(axios.defaults);
 
       this.posts = response.data;
+    } catch (error) {
+      this.errorMessage = error.message;
+    }
+
+    // 用户登录
+    try {
+      const response = await apiHttpClient.post('/login', this.user);
+      this.token = response.data.token;
+
+      console.log(response.data);
     } catch (error) {
       this.errorMessage = error.message;
     }
