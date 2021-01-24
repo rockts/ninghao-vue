65b38a61625772c247dc7aefbd714e425f7c03a5
rockts <rockts@sina.com>
2021-01-25 05:08:22 +0800

    应用的数据管理（Vuex）：解决 Store 模块的类型问题

--- src/app/app.store.ts
+++ src/app/app.store.ts
@@ -1,12 +1,18 @@
 import { createStore } from 'vuex';
-import { postStoreModule } from '../post/post.store';
+import { postStoreModule, PostStoreState } from '../post/post.store';
+
+export interface RootState {
+  appName: string;
+  post: PostStoreState;
+}
+
 /**
  * 创建 Store
  */
 const store = createStore({
   state: {
     appName: '宁浩网',
-  },
+  } as RootState,
 
   modules: {
     post: postStoreModule,
