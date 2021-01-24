5a93299c1f391dc0aba981fc67f915e37999abe6
rockts <rockts@sina.com>
2021-01-24 22:36:51 +0800

    创建与使用 axios 实例

--- src/app/app.vue
+++ src/app/app.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script>
-import { axios } from '@/app/app.service';
+import { apiHttpClient } from '@/app/app.service';
 
 export default {
   data() {
@@ -20,9 +20,9 @@ export default {
 
   async created() {
     try {
-      const response = await axios.get('/posts');
+      const response = await apiHttpClient.get('/posts');
 
-      console.log(axios.defaults);
+      // console.log(axios.defaults);
 
       this.posts = response.data;
     } catch (error) {
