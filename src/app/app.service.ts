b5fd56952389dcbdb4b39a220e655a663118eea3
rockts <rockts@sina.com>
2021-01-25 05:44:06 +0800

    Vue 应用的环境变量配置

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
+import { API_BASE_URL } from './app.config';
 
 export const apiHttpClient = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: API_BASE_URL,
 });
