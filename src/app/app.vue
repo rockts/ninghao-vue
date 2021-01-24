3ff588ffb075ef435e574fc9c26daeda23f0f967
rockts <rockts@sina.com>
2021-01-25 00:59:38 +0800

    创建用户登录组件

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,5 +1,8 @@
 <template>
   <h3>{{ name }}</h3>
+
+  <UserLogin />
+
   <input type="text" v-model="title" @keyup.enter="createPost" />
   <div>{{ errorMessage }}</div>
   <div v-for="post in posts" :key="post.id">
@@ -15,6 +18,7 @@
 
 <script>
 import { apiHttpClient } from '@/app/app.service';
+import UserLogin from '@/user/components/user-login';
 
 export default {
   data() {
@@ -118,6 +122,10 @@ export default {
         this.errorMessage = error.message;
       }
     },
+  },
+
+  components: {
+    UserLogin,
   },
 };
 </script>
