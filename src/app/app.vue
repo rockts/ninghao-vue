1e484aba42b707dee0e2cfcdee7c5eb93c489bc8
rockts <rockts@sina.com>
2021-01-24 17:47:53 +0800

    JavaScript：Proxy 与 Reflect

--- src/app/app.vue
+++ src/app/app.vue
@@ -9,6 +9,38 @@ export default {
       name: '宁浩网',
     };
   },
+
+  created() {
+    const user = {
+      name: '宁浩网',
+    };
+
+    const handler = {
+      get(target, property) {
+        return target[property];
+      },
+
+      set(target, property, value) {
+        if (property === 'name') {
+          if (value.length > 10) {
+            throw new Error('名字太长了！');
+          }
+        }
+
+        return Reflect.set(target, property, value);
+      },
+    };
+
+    const userProxy = new Proxy(user, handler);
+
+    try {
+      userProxy.name = 'NINGHAONINGHAO';
+    } catch (error) {
+      console.log('错误：', error.message);
+    }
+
+    console.log(userProxy.name);
+  },
 };
 </script>
 
