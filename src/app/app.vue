b753c4e296ed9e01cfb1f818fa1fd53d6c303dde
rockts <rockts@sina.com>
2021-01-25 01:24:27 +0800

    获取当前用户数据

--- src/app/app.vue
+++ src/app/app.vue
@@ -7,6 +7,8 @@
     @login-error="onLoginError"
   />
 
+  <div v-if="currentUser">{{ currentUser.name }}</div>
+
   <input
     v-if="isLoggedIn"
     type="text"
@@ -39,6 +41,7 @@ export default {
       errorMessage: '',
       token: '',
       title: '',
+      currentUser: null,
     };
   },
 
@@ -53,8 +56,19 @@ export default {
   },
 
   methods: {
+    async getCurrentUser(userId) {
+      try {
+        const response = await apiHttpClient.get(`/users/${userId}`);
+
+        this.currentUser = response.data;
+      } catch (error) {
+        this.errorMessage = error.message;
+      }
+    },
+
     onLoginSuccess(data) {
       this.token = data.token;
+      this.getCurrentUser(data.id);
     },
 
     onLoginError(error) {
